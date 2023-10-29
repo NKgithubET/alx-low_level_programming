72c7a1ff658dc7862dc4d7a67cf7aa6cdadb4e4f
Nahom <ets079711eeng@aastu.edu.et>
2023-10-29 09:02:31 -0700

    second commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(int argc, char *argv[])
+
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+
+		printf("%s\n",argv[i]);
+
+	
+		i++;
+
+	}
+
+	return(0);
+
+}
